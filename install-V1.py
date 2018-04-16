2cc26515b74ee21615496a478c4a195ec9eec208
Andrus Diaz German <30560543+AndrusGerman@users.noreply.github.com>
2018-04-15 20:24:59 -0400

    installer-V1.0-stable 32 Bits (No Gui)

--- install-V1.py
+++ install-V1.py
@@ -5,7 +5,7 @@ n=str(input('desea instalar el thema de iconos s/n: '))
 if(n=="s"):
     os.system("sudo cp -r osx /usr/share/icons/")
 if(n=="n"):
-    print("cerrando........")
-    time.sleep(2)
+    print("cerrando.......")
+    time.sleep(1)
     os.system("clear")
     exit()
